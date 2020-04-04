5800dfebbfe657846051ae73da3172490598914d
Fabio Tessaro <fabio.tessaro.porta@gmail.com>
2020-04-04 22:09:05 +0200

    Recommit dopo merge

--- sectors.py
+++ sectors.py
@@ -80,7 +80,6 @@ class Corner:
     def __str__(self):
         return ("id: " +str(self.id)+ "\n")
 
-<<<<<<< HEAD
     def roundify(self):
         if self.blend and self.arc_start != None and self.arc_finish != None:
             circle_coords = lambda b : [self.center[0]+self.radius*math.cos(math.radians(b)), self.center[1]+self.radius*math.sin(math.radians(b))]
@@ -94,43 +93,6 @@ class Corner:
             print("AS: "+str(angle_start))
             print("AE: "+str(angle_end))
             print("T: "+str(theta))
-=======
-    def _mod360(self,t):
-        if t > 0:
-            return t%360
-        else:
-            return 360 - (-t%360)
-
-    def _mod180(self, t):
-        if t > 180:
-            print("before: " + str(t))
-            print("after: " + str(t%180 - 180))
-            return t%180 - 180
-        elif t < -180:
-            print("before: " + str(t))
-            print("after: " + str(t%(-180) + 180))
-            return t%(-180) + 180
-        else:
-            print(": " + str(t))
-            return t
-
-    def roundify(self):
-        if self.blend and self.arc_start != None and self.arc_finish != None:
-            circle_coords = lambda b : [self.center[0]+self.radius*math.cos(math.radians(b)), self.center[1]+self.radius*math.sin(math.radians(b))]
-            start_angle = angle_of(self.arc_start)
-            end_angle = angle_of(self.arc_finish)
-            theta = math.degrees(angle_3_points(self.arc_start, self.center, self.arc_finish))
-
-            print("Center: "+str(self.center))
-            print("Radius: "+str(self.radius))
-            print("Start angle: " +str(start_angle))
-            print("End angle: " +str(end_angle))
-            print("Theta: " +str(theta))
-
-            plus = abs((start_angle+theta)%180 - end_angle)%180
-            minus = abs((start_angle-theta)%180 - end_angle)%180
-
->>>>>>> aa1ecfc3d277b5734e95fb52366894b8310c39c0
             anglespace = np.linspace(0, theta, num=ANGLE_RES, endpoint=False)
 
             for b in anglespace:
